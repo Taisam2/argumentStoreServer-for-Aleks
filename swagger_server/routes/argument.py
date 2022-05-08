7718c7a4d47b1dac90edc5f41ff9591ad2de1860
Mathias <mathias.schmid@stud.hslu.ch>
2022-05-08 10:26:07 +0200

    Route & pipeline previously built, is now reversed and deleted, since it is unnecessary.

--- swagger_server/routes/argument.py
+++ swagger_server/routes/argument.py
@@ -7,52 +7,10 @@ from fastapi.encoders import jsonable_encoder
 
 argument = APIRouter()
 
-pipeline = [
-    {
-        '$lookup': {
-            'from': 'argument', 
-            'let': {
-                'solOpt': '$solutionOption', 
-                'argsOpt': '$argumentOption'
-            }, 
-            'pipeline': [
-                {
-                    '$match': {
-                        '$expr': {
-                            '$and': [
-                                {
-                                    '$eq': [
-                                        '$argumentOption', '$$argsOpt'
-                                    ]
-                                }
-                            ]
-                        }
-                    }
-                }
-            ], 
-            'as': 'comments'
-        }
-    }, {
-        '$group': {
-            '_id': '$comments'
-        }
-    }
-]
 
 @argument.get('/getArguments')
 async def findAllArguments():
     return argumentsEntity(conn.local.argument.find())
-
-@argument.get('/getArgumentsByArgOpt')
-async def getSortedAgruments():
-    resultsArr = []
-    results = argumentsEntity(conn.local.argument.aggregate(pipeline))
-    for e in results:
-        print("Array position")
-        print(e)
-        resultsArr.append(e)
-    json_compatible_argument_array = jsonable_encoder(resultsArr)    
-    return json_compatible_argument_array
 
 @argument.post('/addArgument')
 async def createArgument(argument: Argument):
