e3106bd2538ef8ae4d420bac4380993d0244db88
Taisam2 <maschmid2@gmail.com>
2022-06-19 12:56:45 +0200

    Argument-class solutionOption type changed to TopicSolutionOption.

--- argument_store/models/argument/argument.py
+++ argument_store/models/argument/argument.py
@@ -2,14 +2,14 @@ from datetime import datetime
 from pydantic import BaseModel
 from typing import Optional
 
-from argument_store.models.argument.argumentSolutionOption import ArgumentSolutionOption
 from argument_store.models.argument.argumentOptionEnum import ArgumentOptionEnum
+from argument_store.models.topic.topicSolutionOption import TopicSolutionOption
 
 
 class Argument(BaseModel):
     topicId: str
     argumentOption: ArgumentOptionEnum
-    solutionOption: str
+    solutionOption: TopicSolutionOption
     description: str
     date: Optional[datetime] = datetime.now().strftime("%d:%m:%Y %H:%M:%S")
     author: str
