46e4fa6c98b28acde5731e90b1e77c2a5df18c61
Taisam2 <maschmid2@gmail.com>
2022-06-19 14:05:33 +0200

    Tried implementation of error-handling in db.py according to pymongo doc.

--- argument_store/config/db.py
+++ argument_store/config/db.py
@@ -1,4 +1,3 @@
-from fastapi import HTTPException
 from pymongo import MongoClient
 from argument_store.config.util import DbUtil
 
@@ -6,13 +5,16 @@ from argument_store.config.util import DbUtil
 u = DbUtil()
 
 #or not conn.get_database("ArgumentStore").get_collection("local.argument")
+conn_string = "mongodb+srv://" + u.username +":" + u.passwort + "@argumentstore.lkyrg.mongodb.net/?retryWrites=true&w=majority"
 
-conn = MongoClient("mongodb+srv://" + u.username +":" + u.passwort + "@argumentstore.lkyrg.mongodb.net/?retryWrites=true&w=majority")
+conn = MongoClient(conn_string, serverSelectionTimeoutMS=5000)
 
-if not conn:
-    raise HTTPException(status_code=504, detail="Verbindung zur Datenbank unterbrochen.")
-else:
+try:
     print("Server is running with the following properties:\n")
     print(conn.server_info)
+    #raise HTTPException(status_code=504, detail="Verbindung zur Datenbank unterbrochen.")
+except Exception as e:
+    print("DB-connection failed: \n")
+    print(e)
 
 
