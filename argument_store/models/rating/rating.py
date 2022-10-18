6f1bc310e42004d9e42d9a896266d45e66b17a56
Taisam2 <maschmid2@gmail.com>
2022-10-18 15:20:04 +0200

    addRatings route implemented.

--- argument_store/models/rating/rating.py
+++ argument_store/models/rating/rating.py
@@ -1,11 +1,13 @@
 from datetime import datetime
+from xmlrpc.client import boolean
 from pydantic import BaseModel
 from typing import Union
 
 class Rating(BaseModel):
     optionNumber: int
     optionName: str
+    username: str
     sol_rat_1: float
     sol_rat_2: float
-    sol_rat_3: float    
+    sol_rat_3: Union[boolean, None] = False
     date: Union[datetime, None] = datetime.now().strftime("%d:%m:%Y %H:%M:%S")
