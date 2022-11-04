a92d74725e3407fa0a9f35ab60aaa49baaf0926c
Taisam2 <maschmid2@gmail.com>
2022-11-04 12:21:35 +0100

    added csv-export route prototype. Has to be tested with db.

--- argument_store/routes/rating.py
+++ argument_store/routes/rating.py
@@ -1,8 +1,10 @@
+from http.client import HTTPException
 from fastapi import APIRouter
+import pandas
 from argument_store.config.db import DatabaseClient
 from argument_store.models.rating.rating import Rating
 
-from argument_store.schemas.rating import ratingEntity
+from argument_store.schemas.rating import ratingsEntity
 
 from fastapi.encoders import jsonable_encoder
 
@@ -29,7 +31,10 @@ async def addRating(rating: Rating):
 @rating.post('/addRatings')
 async def addRatings(ratings: list[Rating]):
     database_client = client.create_database_client()
+
+
     with database_client.start_session() as session:
+
         rating_collection = client.get_rating_collection()
         with session.start_transaction():
             for rating in ratings:
@@ -37,3 +42,14 @@ async def addRatings(ratings: list[Rating]):
             session.commit_transaction()     
             session.end_session()
     return "Rating erfolgreich hinzugefügt!"
+
+
+@rating.get('export-csv')
+async def exposrtCsv():    
+    database_client = client.create_database_client()
+    with database_client.start_session() as session:
+        rating_collection = client.get_rating_collection()
+        if not rating_collection:
+            raise HTTPException(status_code=204, detail="Keine Ratings vorhanden.")
+        else:
+            return rating_collection.to_csv("ratings-export.csv", ",")
